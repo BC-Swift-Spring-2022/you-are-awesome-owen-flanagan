ad3af9f5c4817a84ba186f064340598ccfd453d2
Owen Flanagan <54592856+owen-flanagan@users.noreply.github.com>
2022-01-31 15:12:35 -0500

    Chap 2.9

--- You are Awesome!/ViewController.swift
+++ You are Awesome!/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    var imageNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         messageLabel.text = ""
@@ -19,20 +21,28 @@ class ViewController: UIViewController {
     
     @IBAction func messageButtonPressed(_ sender: Any) {
         
-        let awesomeMessage = "You are Awesome!"
-        let greatMessage = "You are Great!"
-        let daBombMessage = "You are Da Bomb!"
-        
-        if messageLabel.text == awesomeMessage{
-            messageLabel.text = greatMessage
-            imageView.image = UIImage(named: "image1")
-        }else if messageLabel.text == greatMessage{
-            messageLabel.text = daBombMessage
-            imageView.image = UIImage(named: "image2")
-        }else{
-            messageLabel.text = awesomeMessage
-            imageView.image = UIImage(named: "image0")
+        print(imageNumber)
+        let imageName = "image\(imageNumber)"
+        imageView.image = UIImage(named: imageName)
+        imageNumber = imageNumber + 1
+        if imageNumber == 10{
+            imageNumber = 0
         }
         
+//        let awesomeMessage = "You are Awesome!"
+//        let greatMessage = "You are Great!"
+//        let daBombMessage = "You are Da Bomb!"
+//
+//        if messageLabel.text == awesomeMessage{
+//            messageLabel.text = greatMessage
+//            imageView.image = UIImage(named: "image1")
+//        }else if messageLabel.text == greatMessage{
+//            messageLabel.text = daBombMessage
+//            imageView.image = UIImage(named: "image2")
+//        }else{
+//            messageLabel.text = awesomeMessage
+//            imageView.image = UIImage(named: "image0")
+//        }
+//
     }
 }
